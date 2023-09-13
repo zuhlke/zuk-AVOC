4b3019227d51d857c5ecbd432014dfaa73ab62b9
RosscoMT <ross.viviani@zuhlke.com>
2023-09-13 12:41:08 +0100

    Add supported platforms and versions

--- Package.swift
+++ Package.swift
@@ -5,10 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "Verifytool",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "0.50600.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
