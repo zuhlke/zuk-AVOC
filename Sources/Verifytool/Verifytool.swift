454f707b1d3e9a6e760499698a8ad96ccce8ab50
RosscoMT <ross.viviani@zuhlke.com>
2023-09-18 13:06:59 +0100

    Add the verbose and inputFile options

--- Sources/Verifytool/Verifytool.swift
+++ Sources/Verifytool/Verifytool.swift
@@ -6,6 +6,11 @@ import ArgumentParser
 struct Verifytool: ParsableCommand {
     static let configuration = CommandConfiguration(commandName: "Verify Tool",
             abstract: "Welcome to the Zuhlke Swift Verification Tool. To help up hold our high development quality and assurance for our customers, we have designed this tool to check the compliance of your code")
+    @Option(name: .shortAndLong, help: "The file you want reviewed")
+    var inputFile: String
+    
+    @Flag(name: .shortAndLong)
+    var verbose = false
     public private(set) var text = "Hello, World!"
 
     public static func main() {
